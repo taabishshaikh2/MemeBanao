fb0e686abd56572255affc025733b2dd5cbf8b89
Taabish Shaikh <taabishshaikh2@users.noreply.github.com>
2021-07-15 22:14:21 +0530

    loading bug fixed

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@
 import './App.css';
 import Meme from './Meme';
 import Navbar from './Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import MemeGenerated from './MemeGenerated';
 
 function App() {
   return (
     <Router>
     <div className="App">
-      
-      <Navbar />
-    <Switch>
-      
-        <Route exact path='/'>
-      <Meme />
-      </Route>
-      <Route path='/generate'>
-        <MemeGenerated />
-      </Route>
-      </Switch>
-      
+    <Navbar/>
+      <div className="container">
+        <Switch>
+          <Route exact path='/'>
+          <Meme />
+          </Route>
+          <Route path='/generate'>
+            <MemeGenerated />
+          </Route>
+          </Switch>
+      </div>
     </div>
 </Router>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,7 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
+
 const Navbar = () => {
     return ( 
         <nav className="nnavbar navbar-dark bg-dark">
